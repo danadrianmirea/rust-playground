09f221957234a5edc00f579396651a5e803cc96b
Dan Adrian Mirea <damirea84@gmail.com>
2025-12-05 07:35:06 +0200

    add some examples of simple functions

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,34 @@
+fn test() {
+    println!("Test fn")
+}
+
+fn add(a: u32, b: u32) -> u32 {
+    return a + b;
+}
+
+fn for_test() {
+    for i in 0..10 {
+        print!("{} ", i);
+    }
+    println!();
+}
+
+fn while_loop() {
+    let mut c = 1;
+
+    while c <= 5 {
+        print!("{} ", c);
+        c = c + 1;
+    }
+    println!();
+}
+
 fn main() {
-    println!("Test, Hello World Rust.");
+    //test();
+
+    //let res = add(2, 3);
+    //println!("res is: {}", res);
+
+    //for_test();
+    while_loop();
 }
