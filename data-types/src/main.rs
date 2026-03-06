0d7727f0f6781f0107be688f9b3e401046e70df1
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-06 17:08:23 +0200

    add data types example

---
+++ data-types/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+  // int, float, bool, char
+  // u8, u16, ... , u128  
+  // i8, i16, ... , i128
+  
+  let x: i32 = -32;
+  let y: u64 = 100;
+  
+  println!("x: {}", x);
+  println!("y: {}", y);
+    
+}
