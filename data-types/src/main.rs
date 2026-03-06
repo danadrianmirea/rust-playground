66915c9c8526f9f2a898151b32afc2dfeff9979d
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-06 21:26:37 +0200

    add tuples and strings

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -18,6 +18,19 @@ fn main() {
   // arrays
   let arr: [i32; 5] = [1, 2, 3, 4, 5];
   println!("arr: {:?}", arr);
-  
+
+  let mixed_arr: [i32; 5] = [0; 5]; // initializes all elements to 0
+  println!("mixed_arr: {:?}", mixed_arr);
+
+  // tuples
+  let tup: (i32, f64, char) = (42, 3.14, 'R');
+  println!("tup: {:?}", tup);
+
+  // strings
+  let s: String = String::from("Hello, Rust!");
+  println!("s: {}", s);
+
+   
+
     
 }
