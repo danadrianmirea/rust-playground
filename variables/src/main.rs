bb85dee1066f30433e96dc611d494619ba0eabcf
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-06 19:38:27 +0200

    add mutable char variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,8 +4,8 @@ fn main() {
     let my_integer = 3;
     let default_float = 3.29;
     let default_integer = 8;
-
     let my_array: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut letter: char = 'A';
 
     println!("logical: {}", logical);
     println!("a_float: {}", a_float);
@@ -13,5 +13,8 @@ fn main() {
     println!("default_float: {}", default_float);
     println!("default_integer: {}", default_integer);
     println!("my_array: {:?}", my_array);
+    println!("letter: {}", letter);
+    letter = 'B';
+    println!("letter: {}", letter);
 
 }
