601e7917a86f13bd554d9cd6851b4226308a8972
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-17 19:40:08 +0200

    add a fucntion with parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,12 @@ fn helloworld() {
     println!("Hello, world!");
 }
 
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
 fn main() {
     helloworld();
+    let sum = add(5, 10);
+    println!("The sum of 5 and 10 is: {}", sum);
 }
