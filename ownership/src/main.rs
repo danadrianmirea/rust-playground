35e794a864ef6b041507b9bbc501e2d76b37835e
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-17 20:29:15 +0200

    add some mutable/imutable examples

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,6 +10,15 @@ fn main() {
 
     let s3 = s2.clone(); // this creates a deep copy of the string, so both s2 and s3 have ownership of their own string data
     println!("s2: {}, s3: {}", s2, s3);
+
+    let mut x: i32 = 5;
+    let y: &mut i32 = &mut x; // this creates a mutable reference to x, but since i32 is a Copy type, we can still use x after this without any issues
+    *y += 1; // this modifies the value of x through the mutable reference y
+    
+    println!("x: {}", x);
+    //println!("y: {}", y); // can only have one mutable reference to a value at a time, so this would give an error if we tried to use y after modifying x through it
+
+
 }
 
 fn calculate_length(s: &String) -> usize {
