bd23b3a973b76deaac07afbb1dc9d14832eb8e1f
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-17 20:03:52 +0200

    add deep copy/clone example

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,6 +7,9 @@ fn main() {
     let s2 = s1; // this moves ownership of the string from s1 to s2
     //println!("s1: {}", s1); -> this gives an ownership error because s1 is no longer valid after the move
     println!("s2: {}", s2);
+
+    let s3 = s2.clone(); // this creates a deep copy of the string, so both s2 and s3 have ownership of their own string data
+    println!("s2: {}, s3: {}", s2, s3);
 }
 
 fn calculate_length(s: &String) -> usize {
