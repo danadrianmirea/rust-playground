39d4fe6a157f09555ccd95daf4e1e584187ee993
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-17 19:51:00 +0200

    add calculate bmi function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -6,6 +6,10 @@ fn add(a: i32, b: i32) -> i32 {
     return a + b;
 }
 
+fn calculate_bmi(weight: f32, height: f32) -> f32 {
+    return weight / (height * height);
+}
+
 fn main() {
     helloworld();
     let sum = add(5, 10);
@@ -17,4 +21,9 @@ fn main() {
         y+z
     };
     println!("The value of x is: {}", x);
+
+    let weight = 70.0; // in kilograms
+    let height = 1.75; // in meters
+    let bmi = calculate_bmi(weight, height);
+    println!("The BMI is: {:.2}", bmi);
 }
