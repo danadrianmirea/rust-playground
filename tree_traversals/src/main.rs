302653a23cc2877c19582784be4f57d6f841abc3
Dan Adrian Mirea <damirea84@gmail.com>
2026-03-30 23:59:46 +0300

    update tree traversal code

--- tree_traversals/src/main.rs
+++ tree_traversals/src/main.rs
@@ -3,30 +3,31 @@ use std::collections::VecDeque;
 #[derive(Clone)]
 struct TreeNode {
     value: i32,
-    left: Option<Box<TreeNode>>,
-    right: Option<Box<TreeNode>>,
+    children: Vec<Box<TreeNode>>,
 }
 
 fn main() {
     let root = generate_example_tree();
+    
     println!("Breadth First Traversal:");
     breadth_first_traversal(root.clone());
+    
     println!("Depth First Traversal:");
     depth_first_traversal(root);
 }
 
-// Breadth First Traversal
+// Breadth First Traversal (level-order)
 fn breadth_first_traversal(root: Option<Box<TreeNode>>) {
     if let Some(node) = root {
         let mut queue = VecDeque::new();
         queue.push_back(node);
+
         while let Some(node) = queue.pop_front() {
             println!("{}", node.value);
-            if let Some(left) = node.left {
-                queue.push_back(left);
-            }
-            if let Some(right) = node.right {
-                queue.push_back(right);
+
+            // Push all children into the queue
+            for child in node.children.iter() {
+                queue.push_back(child.clone());
             }
         }
     }
@@ -36,22 +37,26 @@ fn breadth_first_traversal(root: Option<Box<TreeNode>>) {
 fn depth_first_traversal(root: Option<Box<TreeNode>>) {
     if let Some(node) = root {
         println!("{}", node.value);
-        depth_first_traversal(node.left);
-        depth_first_traversal(node.right);
+
+        for child in node.children.iter() {
+            depth_first_traversal(Some(child.clone()));
+        }
     }
 }
 
-// Generate an example tree:
-//        1
-//       / \
-//      2   3
+// Generate an example generic tree
+//          1
+//       /  |  \
+//      2   3   4
 //     / \
-//    4   5
+//    5   6
 fn generate_example_tree() -> Option<Box<TreeNode>> {
-    let node4 = Some(Box::new(TreeNode { value: 4, left: None, right: None }));
-    let node5 = Some(Box::new(TreeNode { value: 5, left: None, right: None }));
-    let node2 = Some(Box::new(TreeNode { value: 2, left: node4, right: node5 }));
-    let node3 = Some(Box::new(TreeNode { value: 3, left: None, right: None }));
-    let root = Some(Box::new(TreeNode { value: 1, left: node2, right: node3 }));
-    root
+    let node5 = Box::new(TreeNode { value: 5, children: vec![] });
+    let node6 = Box::new(TreeNode { value: 6, children: vec![] });
+    let node2 = Box::new(TreeNode { value: 2, children: vec![node5, node6] });
+    let node3 = Box::new(TreeNode { value: 3, children: vec![] });
+    let node4 = Box::new(TreeNode { value: 4, children: vec![] });
+
+    let root = Box::new(TreeNode { value: 1, children: vec![node2, node3, node4] });
+    Some(root)
 }
